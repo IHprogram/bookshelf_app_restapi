cd953c12fce860ba4c6b07e18f98988f8fc828a5
IHprogram <ihm20202020@gmail.com>
2021-09-21 16:17:13 +0900

    ホストURLの追加

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -9,7 +9,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+# URLを追加
+ALLOWED_HOSTS = ['bookshelf-app-restapi.herokuapp.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
