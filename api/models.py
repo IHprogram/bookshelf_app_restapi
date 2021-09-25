8c58d6d685e9a3767b292278cc06da7a2a0fb55a
IHprogram <ihm20202020@gmail.com>
2021-09-25 22:18:39 +0900

    Bookモデルの作成

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    image = models.CharField(max_length=500)
+    caption = models.CharField(max_length=500)
+    itemUrl = models.CharField(max_length=500)
+    loginUserId = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id) + " - " + self.title
