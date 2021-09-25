840d3d1b0618380e49021ced3d49e8fe51c52a72
IHprogram <ihm20202020@gmail.com>
2021-09-25 22:30:32 +0900

    登録した書籍情報に関するURLの設定

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
-from api.views import CreateUserView
+from api.views import CreateUserView, BookListView, BookRetrieveView
 
 router = routers.DefaultRouter()
 
 urlpatterns = [
+    path('list-book/', BookListView.as_view(), name='list-book'),
+    path('detail-book/<str:pk>/', BookRetrieveView.as_view(), name='detail-book'),
     path('register/', CreateUserView.as_view(), name='register'),
     path('auth/', include('djoser.urls.jwt')),
     path('', include(router.urls)),
