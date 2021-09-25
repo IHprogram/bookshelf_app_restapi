2ddea7274f64b0265e1c9470e86fd35fc5c11b9c
IHprogram <ihm20202020@gmail.com>
2021-09-25 22:27:37 +0900

    登録した書籍情報に関するViewsを定義

--- api/views.py
+++ api/views.py
@@ -2,7 +2,18 @@ from rest_framework.permissions import AllowAny
 from rest_framework import generics
 from rest_framework import viewsets
 from .serializers import UserSerializer
+from .models imoport Book
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = (AllowAny,)
+
+class BookRetrieveView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
     permission_classes = (AllowAny,)
