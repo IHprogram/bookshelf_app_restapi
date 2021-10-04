f0cf10aa53ed94108f65fb1d425ef7ceca7b0ebe
IHprogram <ihm20202020@gmail.com>
2021-10-04 16:06:36 +0900

    書籍情報保存処理を行うURLを変更

--- api/urls.py
+++ api/urls.py
@@ -9,7 +9,7 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path('list-book/', BookListView.as_view(), name='list-book'),
     path('detail-book/<str:pk>/', BookRetrieveView.as_view(), name='detail-book'),
-    path('post/new/', CreateBookView.as_view(), name='book_new'),
+    path('book-new/', CreateBookView.as_view(), name='book-new'),
     path('register/', CreateUserView.as_view(), name='register'),
     path('auth/', include('djoser.urls.jwt')),
     path('', include(router.urls)),
